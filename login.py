ebbaa49f59d3747051fc441f0933b30b65a1b697
knpehehko <34830336+knpehehko@users.noreply.github.com>
2018-01-05 19:13:25 +0800

    登陆注销

--- login.py
+++ login.py
@@ -1,66 +1,75 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, redirect, url_for, escape, request
 from flask import make_response
 from flask import render_template
+from db_manage import User
+from flask import jsonify,abort
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
+users = [ ]
+
+#wjw的函数
+def add_token():
+#get_users()
+#增加id
+    return None
+
+def delete_token():
+#get_users()
+#删掉id
+    return None
+
+
+#tokenid
+def generate_auth_token(self, expiration=3600):
+    s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+    return s.dumps({'id': 2 })
 
 #登陆
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'POST':
-        name = request.form['username']
+        user=User()
+        user.name= request.form['username']
+        password=user.login()
         passwd = request.form['password']
-        user_file = open('account.txt','r')                             #打开帐号文件
-        user_list = user_file.readlines()
-        for user_line in user_list:                                     #对帐号文件进行遍历
-            (user,password) = user_line.strip('\n').split()             #分别获取帐号和密码信息
-            if name == user:                                            #如用户名正常匹配
-                if passwd == password:                                  #密码正确
-                    user_file.close()                                   #关闭帐号文件
-                    session['username'] = request.form['username']
-                    resp = make_response(render_template('login.html'))
-                    resp.set_cookie('username',name)#设置cookies
-                    return resp
-                return 'Password is wrong!'
-        return 'User not exist!'
+    
+        if password == "mei you zhe ren":
+            return 'user not exist'
+        if passwd == password:                                  #密码正确
+            tokenid = str(generate_auth_token(3600))
+            
+            user = {
+                'username': user.name,
+                'id': 1,
+                'password': password,
+                'tokenid': tokenid
+            }
+            
+            
+            users.append(user)
+            add_token()#wjw
+
+            return 'success'
+        return 'Password is wrong!'
     return render_template('login.html')#get和其它请求
 
-#
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify({'users': users})
+
+
+#注销
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
+    delete_token()#wjw
 
-    return redirect(url_for("delete_cookie"))
-
-# set the secret key.  keep this really secret:
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
-
-#删除cookie
-@app.route("/delete_cookies")
-def delete_cookie():
-    resp = make_response("delete cookie ok")
-    resp.delete_cookie('username')
-    return resp
-
-#获取cookie
-@app.route('/get_cookies')
-def test_cookies():
-    if request.method == 'GET':
-        username = request.cookies.get('username',None)
-    if username:
-        return 'welcome {0}'.format(username)
-    else:
-        return 'Please login first.'
-
-
+    return 'user logout.'
+#
 
 if __name__ == '__main__':
     app.run()
